0967c5403dc732339feda33cbb01c1fd884b2098
jimfengnpu <jimfelpos@163.com>
2023-01-15 23:32:10 +0800

    change ramfs find_path to iteration, rename some vars

--- kernel/vfs.c
+++ kernel/vfs.c
@@ -96,8 +96,8 @@ void init_fileop_table()
     f_op_table[3].read = rf_read;
     f_op_table[3].lseek = rf_lseek;
     //     f_op_table[3].opendir = rf_openDir;
-    f_op_table[3].createdir = rf_createDir;
-    f_op_table[3].deletedir = rf_deleteDir;
+    f_op_table[3].createdir = rf_create_dir;
+    f_op_table[3].deletedir = rf_delete_dir;
 
 }
 
