4c5f82dc6e4b1890a79a4b8e86a40a90070bc5f0
TankTechnology <2541826291@qq.com>
2023-02-04 17:16:21 +0800

    do some test in shell_0.c

--- user/shell_0.c
+++ user/shell_0.c
@@ -50,7 +50,7 @@
 char cmd_buf[MAX_CMD_LEN];
 #define TMP_BUF_LEN 600
 char tmp[TMP_BUF_LEN];
-#define DATA_BUF_LEN 600
+#define DATA_BUF_LEN 2000005
 char data_buf[DATA_BUF_LEN];
 
 int pwd_u() {
@@ -135,10 +135,12 @@ int cat_u() {
 	else if(ret >= 0) {
 		cmd_buf[ret] = '\0';
 		printf("%s", cmd_buf);
-        printf("\n data len: %d\n", ret);
+		printf("\ncat finished\n");
 		close(fd);
-    } else 
+    } else {
+		close(fd);
 		return -1;
+	}
     
 	return 0;
 }
@@ -185,40 +187,21 @@ int (*cmd_table[CMD_NUM])() = {
 	ls_u,
 };
 
-void easytest() {
-	strcpy(cmd_buf, "mkdir ram/test");
-	mkdir_u();
-	strcpy(cmd_buf, "cd ram/test");
-	chdir_u();
-	strcpy(cmd_buf, "touch a");
-	touch_u();
-	strcpy(cmd_buf, "write a hello world");
-    printf("started write\n");
-	write_u();
-	strcpy(cmd_buf, "cat a");
-	cat_u();
 
-
-	// printf("hello, this is file test\n");
-	// int fd = open("ram/test.txt", O_RDWR | O_CREAT);
-	// if (fd == -1) {
-	// 	printf("open failed\n");
-	// 	return;
-	// }
-	// int ret = write(fd, "hello world", 11);
-	// if (ret == -1) {
-	// 	printf("write failed\n");
-	// 	return;
-	// }
-	// ret = lseek(fd, 0, SEEK_SET);
-	// ret = read(fd, cmd_buf, 11);
-	// cmd_buf[11] = '\0';
-	// if (ret == -1) {
-	// 	printf("read failed\n");
-	// 	return;
-	// }
-	// printf("%s\n", cmd_buf);
-	// printf("test finished\n");
+void cmd_parser() {
+	int i;
+	for (i = 0; i < CMD_NUM; i++)
+	{
+		if(strlen(cmd_name[i]) <= strlen(cmd_buf) && strncmp(cmd_name[i], cmd_buf, strlen(cmd_name[i])) == 0)
+		{
+			cmd_table[i]();
+			break;
+		}
+	}
+	if (i == CMD_NUM)
+	{
+		printf("command not found\n");
+	}
 }
 
 void fake_shell() {
@@ -227,31 +210,134 @@ void fake_shell() {
 		printf("\nminiOS:/ $ ");
 		if (gets(cmd_buf) && strlen(cmd_buf) != 0)
 		{
-			int i;
-			for (i = 0; i < CMD_NUM; i++)
-			{
-				if(strlen(cmd_name[i]) <= strlen(cmd_buf) && strncmp(cmd_name[i], cmd_buf, strlen(cmd_name[i])) == 0)
-				{
-					cmd_table[i]();
-					break;
-				}
-			}
-			if (i == CMD_NUM)
-			{
-				printf("command not found\n");
-			}
+			cmd_parser();
 		}
 	}
 }
 
+void cmd_dostr(char *str) {
+	strcpy(cmd_buf, str);
+	printf("test : %s\n", cmd_buf);
+	cmd_parser();
+}
+
+// 进行基本的功能测试
+void easytest() {
+	cmd_dostr("mkdir ram");
+	cmd_dostr("cd ram");
+	cmd_dostr("touch a");
+	cmd_dostr("write a hello world");
+	cmd_dostr("cat a");
+	cmd_dostr("write a change");
+	cmd_dostr("cat a");
+	cmd_dostr("cd /ram");
+	cmd_dostr("mkdir b");
+	cmd_dostr("mkdir c");
+	// 检查是否会访问错误文件
+	cmd_dostr("cd d");
+	fake_shell();
+}
+// 高压力读写测试
+void high_rw_test() {
+	// 全部写入字符'a'
+	for(int i = 0;i < 2e6;i++) data_buf[i] = 'a';
+	data_buf[2000000] = '\0';
+	int start_ticks = get_ticks();
+	cmd_dostr("mkdir ram");
+	cmd_dostr("cd ram");
+	printf("write 2e6 bytes to file \"ram/all_a\"\n");
+	int fd = open("all_a", O_CREAT | O_RDWR);
+	lseek(fd, 0, SEEK_SET);
+	int ret = -1;
+	if((ret = write(fd, data_buf, strlen(data_buf))) != 2000000) {
+		printf("write failed\n");
+	} else {
+		printf("successfully written\n");
+	}
+	// 随机读测试
+	lseek(fd, 0, SEEK_SET);
+	// 读入1000个bytes检查
+	char tmp[10];
+	int cnt = 0;
+	while(read(fd, tmp, 1) == 1 && cnt != 1000) {
+		cnt++;
+		if(tmp[0] != 'a') {
+			printf("find a read problem in 'all_a' test\n");
+			return;
+		}
+	}
+	if(cnt != 1000) {
+		printf("test error\n");
+		return;
+	}
+	printf("\"Read 1000 bytes from head\" test is passed\n");
+	lseek(fd, 114514, SEEK_CUR); // 随机读取测试
+	cnt = 0;
+	while(read(fd, tmp, 1) == 1 && cnt != 1000) {
+		cnt++;
+		if(tmp[0] != 'a') {
+			printf("find a read problem in 'all_a' test\n");
+			return;
+		}
+	}
+	if(cnt != 1000) {
+		printf("test error\n");
+		return;
+	}
+	printf("\"Randomly read 1000 bytes from head\" test is passed\n");
+	lseek(fd, -5000, SEEK_END);
+	cnt = 0;
+	while(read(fd, tmp, 1) == 1 && cnt != 1000) {
+		cnt++;
+		if(tmp[0] != 'a') {
+			printf("find a read problem in 'all_a' test\n");
+			return;
+		}
+	}
+	if(cnt != 1000) {
+		printf("test error\n");
+	}
+	printf("\"READ 1000 bytes from end\" test is passed\n");
+	int end_ticks = get_ticks();
+	close(fd);
+	printf("all_a test is passed\n");
+	printf("This test use %d ticks\n", end_ticks-start_ticks);
+	cmd_dostr("cd /");
+	fd = open("orange/test", O_CREAT | O_RDWR);
+	lseek(fd, 0, SEEK_SET);
+	start_ticks = get_ticks();
+	write(fd, data_buf, strlen(data_buf));
+	end_ticks = get_ticks();
+	printf("Wriet 2e6 bytes to hard disk: This test use %d ticks\n", end_ticks-start_ticks);
+}
+
+// int fattest() {
+// 	char filename[] = "orange/test";
+// 	int fd = open(filename, O_CREAT | O_RDWR);
+// 	if(fd == -1) {
+// 		printf("open failed\n");
+// 	} else {
+// 		printf("oepn %s success\n", filename);
+// 	}
+// 	lseek(fd, 0, SEEK_SET);
+// 	write(fd, "hello world", strlen("hello world"));
+// 	lseek(fd, 0, SEEK_SET);
+// 	read(fd, data_buf, DATA_BUF_LEN);
+// 	printf("data buf : %s\n", data_buf);
+// 	return 0;
+// }
 
 int main(int argc, char *argv[])
 {
 	int stdin = open("dev_tty0", O_RDWR);
 	int stdout = open("dev_tty0", O_RDWR);
 	int stderr = open("dev_tty0", O_RDWR);
-	fake_shell();
+	// fattest();
+	// fake_shell();
 	// easytest();
+	high_rw_test();
+
+
 	// testDir();
 	// while(1) {
 	// 	fake_shell();
